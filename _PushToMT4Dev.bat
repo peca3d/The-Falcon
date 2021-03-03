e53ecdded61f3ea517378971f186113e93e138a8
maneerat3d <maneerat3d@gmail.com>
2021-03-03 17:28:50 +0700

    Update _PushToMT4Dev.bat
    เพิ่ม Path Environment

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -5,9 +5,10 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 setlocal enabledelayedexpansion
 
 :: Source Directory where Version Control Repository is located
-set SOURCE_DIR="C:\MT4\DEV\The-Falcon"
+set SOURCE_DIR="%PATH_DSS_Repo%\The-Falcon"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="C:\MT4\MetaTrader - EXNESS2\MQL4\Experts\The-Falcon"
+set DEST_DIR="%PATH_T2_E%\The-Falcon"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 
+pause
